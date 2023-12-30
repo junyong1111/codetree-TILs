7d4f9f131b531a52b0626cb95288ae41bf6b149e
Junyong <79856225+junyong1111@users.noreply.github.com>
2023-12-30 19:53:40 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231230/20대의 평균나이/average-age-of-20.py
+++ 231230/20대의 평균나이/average-age-of-20.py
@@ -2,14 +2,12 @@ import sys
 import math
 
 ages = []
-cnt= 100
 while cnt:
-    cnt-=1
     n = int(sys.stdin.readline())
-    if n >= 30:
-        break
-    else:
+    if n>=20 and n<=29:
         ages.append(n)
+    else:
+        break
 mean = sum(ages) / len(ages)
 
 print("%.2f"%mean)
