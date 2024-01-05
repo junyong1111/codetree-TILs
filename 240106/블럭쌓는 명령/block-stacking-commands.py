3e14e76fc49676c8067f695f49d52e0e232675c7
Junyong <79856225+junyong1111@users.noreply.github.com>
2024-01-06 08:05:10 +0900

    Title: 블럭쌓는 명령, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240106/블럭쌓는 명령/block-stacking-commands.py
@@ -0,0 +1,12 @@
+import sys
+# sys.stdin = open("input.txt")
+
+N, K = map(int, sys.stdin.readline().split())
+block = [0] * (N+1)
+
+for _ in range(K):
+    a,b = map(int, sys.stdin.readline().split())
+    for i in range(a, b+1):
+        block[i] +=1
+block.sort()
+print(block[round(N/2)])
