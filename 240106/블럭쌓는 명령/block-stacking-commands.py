84bf8abdd33171e584c616d24d0996a1f6193bd6
Junyong <79856225+junyong1111@users.noreply.github.com>
2024-01-06 15:08:26 +0900

    Title: 블럭쌓는 명령, Time: 191ms, Memory: 46MB, Status: Passed - CodeTree

--- 240106/블럭쌓는 명령/block-stacking-commands.py
+++ 240106/블럭쌓는 명령/block-stacking-commands.py
@@ -2,11 +2,20 @@ import sys
 # sys.stdin = open("input.txt")
 
 N, K = map(int, sys.stdin.readline().split())
-block = [0] * (N+1)
+diff = [0] * (N + 1)  # 누적 합을 계산하기 위한 차분 배열
 
 for _ in range(K):
-    a,b = map(int, sys.stdin.readline().split())
-    for i in range(a, b+1):
-        block[i] +=1
+    a, b = map(int, sys.stdin.readline().split())
+    diff[a-1] += 1
+    diff[b] -= 1
+
+# 누적 합 계산
+block = [0] * (N)
+current = 0
+for i in range(N):
+    current += diff[i]
+    block[i] = current
+# 블록 배열 정렬
 block.sort()
-print(block[(N//2)+1])
+# 중간 값 출력
+print(block[N // 2])
