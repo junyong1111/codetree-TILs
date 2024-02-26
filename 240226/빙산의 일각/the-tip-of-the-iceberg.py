3d1fd8884f49d1662ef3f27cc06df7cb182731db
Junyong <79856225+junyong1111@users.noreply.github.com>
2024-02-26 15:37:13 +0900

    Title: 빙산의 일각, Time: 5025ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240226/빙산의 일각/the-tip-of-the-iceberg.py
+++ 240226/빙산의 일각/the-tip-of-the-iceberg.py
@@ -6,20 +6,26 @@ heights = []
 for _ in range(N):
     heights.append(int(sys.stdin.readline()))
 
-heights_sort = sorted(set(heights), reverse=True)
-    
-def Iceberg(target):
-    flag = False
-    cnt = 0
-    for height in heights:
-        if target < height and flag == False:
-            cnt+=1
-            flag = True
-        elif target >= height and flag == True:
-            flag = False
-    return cnt     
+def optimized_iceberg_groups(heights):
+    unique_heights = sorted(set(heights), reverse=True)  # 고유 높이 정렬
+    max_groups = 0
+    index = 0  # 현재 높이의 인덱스
 
-answer = 0
-for height_sort in heights_sort:
-    answer = max(answer, Iceberg(height_sort))
-print(answer)
+    while index < len(unique_heights):
+        water_level = unique_heights[index]
+        groups = 0
+        in_group = False
+        for height in heights:
+            if height > water_level:
+                if not in_group:
+                    groups += 1
+                    in_group = True
+            else:
+                in_group = False
+        max_groups = max(max_groups, groups)
+        index += 1
+
+    return max_groups
+
+# 다시 최적화된 방식으로 최대 빙산 덩어리의 수 계산
+print(optimized_iceberg_groups(heights))
