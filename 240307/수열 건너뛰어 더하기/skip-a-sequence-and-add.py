8238bb86c163562117ca6779e8d270890595966b
Junyong <79856225+junyong1111@users.noreply.github.com>
2024-03-07 15:24:55 +0900

    Title: 수열 건너뛰어 더하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/수열 건너뛰어 더하기/skip-a-sequence-and-add.py
+++ 240307/수열 건너뛰어 더하기/skip-a-sequence-and-add.py
@@ -6,13 +6,14 @@ MAX_SIZE = 301
 elemetns = [0] * (n+1)
 for i in range(1, n+1):
     elemetns[i] = int(sys.stdin.readline())
-    
-dp = [0] * (MAX_SIZE)
-dp[1] = elemetns[1]
-dp[2] = elemetns[1] + elemetns[2]
-dp[3] = elemetns[3] + max(elemetns[2], elemetns[1])
 
-for i in range(4, n+1):
-    dp[i] = elemetns[i] + max((dp[i-3] + elemetns[i-1]), dp[i-2])
+dp = [[0 for _ in range(MAX_SIZE)] for _ in range(3)]
+dp[0][1] = elemetns[1] #-- 1개 선택
+dp[1][1] = elemetns[1] #-- 2개 선택
 
-print(dp[n])
+for i in range(2, n+1):
+    dp[0][i] = dp[2][i-1] + elemetns[i]
+    dp[1][i] = dp[0][i-1] + elemetns[i]
+    dp[2][i] = max(dp[0][i-1], dp[1][i-1])
+
+print(max(dp[0][n], dp[1][n]))
