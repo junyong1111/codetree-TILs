a4a724383cac8949ef9f77ee799157af125d4979
Junyong <79856225+junyong1111@users.noreply.github.com>
2024-03-07 16:27:23 +0900

    Title: 가장 큰 증가 부분 수열, Time: 131ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240307/가장 큰 증가 부분 수열/largest-increasing-subsequence.py
+++ 240307/가장 큰 증가 부분 수열/largest-increasing-subsequence.py
@@ -2,23 +2,35 @@ import sys
 # sys.stdin = open("input.txt")
 
 n = int(sys.stdin.readline())
+
 elemetns = list(map(int, sys.stdin.readline().split()))
 
-maxValue = 0
-def backtracking(node, index, level, sumValue):
-    global maxValue
-    maxValue = max(maxValue, sumValue)
-    if level == n:
-        return
-    else:
-        for i in range(index, n):
-            if elemetns[i] >= node: #-- 다음 노드의 값이 더 크다면(증가하는 부분이라면)
-                sumValue += elemetns[i]
-                backtracking(elemetns[i], i+1, level+1, sumValue)
-                sumValue -= elemetns[i]
-     
-for i in range(n):
-    sumValue = elemetns[i]
-    backtracking(elemetns[i], i+1, 0, sumValue) 
+dp = [[0 for _ in range(n+1)] for _ in range(4)]
 
-print(maxValue)
+dp[0][1] = elemetns[0] #-- 원소 선택
+dp[1][1] = elemetns[0] #-- 원소 노선택
+dp[2][1] = elemetns[0] #-- 선택한 경우 노드 값
+dp[3][1] = elemetns[0] #-- 선택안한 경우 노드 값
+
+
+for i in range(2, n+1):
+    if elemetns[i-1]>=dp[2][i-1]:
+        dp[0][i] = dp[0][i-1] + elemetns[i-1]
+        dp[2][i] = elemetns[i-1]
+        
+        dp[1][i] = dp[1][i-1]
+        dp[3][i] = dp[3][i-1]
+    elif elemetns[i-1] >= dp[3][i-1]:
+        dp[0][i] = dp[0][i-1]
+        dp[2][i] = dp[2][i-1]
+        
+        dp[1][i] = dp[1][i-1] + elemetns[i-1]
+        dp[3][i] = elemetns[i-1]
+    else:
+        dp[0][i] = dp[0][i-1]
+        dp[1][i] = dp[1][i-1]
+        dp[2][i] = dp[2][i-1]
+        dp[3][i] = dp[3][i-1]
+
+
+print(max(dp[0][n], dp[1][n]))
