13d787b969a6a22e8da4222f0a1f235e18c0daf9
Junyong <79856225+junyong1111@users.noreply.github.com>
2025-04-23 09:55:26 +0900

    Title: 피보나치 수, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250423/피보나치 수/fibonacci-number.py
+++ 250423/피보나치 수/fibonacci-number.py
@@ -1,8 +1,13 @@
 N = int(input())
+dp_table = [-1] * (46)
+
+dp_table[0] = 0
+dp_table[1] = 1
 
 def fibo(n):
-    if n<=1:
-        return n
-    return fibo(n-1) +  fibo(n-2)
+    if dp_table[n] != -1:
+        return dp_table[n]
+    dp_table[n] = fibo(n-1) + fibo(n-2)
+    return dp_table[n]
 
 print(fibo(N))
