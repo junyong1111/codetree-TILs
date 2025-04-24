286cbedc0ad500fed3c757a8021f8f5ccd3cbd90
Junyong <79856225+junyong1111@users.noreply.github.com>
2025-04-24 10:24:52 +0900

    Title: 사각형 채우기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250424/사각형 채우기/rectangle-fill.py
+++ 250424/사각형 채우기/rectangle-fill.py
@@ -1,4 +1,5 @@
 N = int(input())
+MOD = 10007
 MAX_SIZE = 1001
 
 table = [0] * MAX_SIZE
@@ -6,5 +7,5 @@ table[1] = 1
 table[2] = 2
 
 for i in range(3, N+1):
-    table[i] = table[i-2] + table[i-1]
+    table[i] = (table[i-2] + table[i-1]) % MOD
 print(table[N])
